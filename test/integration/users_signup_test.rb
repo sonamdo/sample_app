ee5968f8f6d9aed05b4deae303e77305f0a339ea
Sonam <sonamklein@gmail.com>
2021-06-08 10:23:06 -0400

    finish user signup

---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,19 @@
+require "test_helper"
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+  test "invalid signup information" do
+    get signup_path
+
+    assert_no_difference 'User.count' do
+      post users_path, params: { user: { name:  "", email: "user@invalid", password:"foo", password_confirmation: "bar" } }
+    end
+
+    assert_difference 'User.count', 1 do
+      post users_path, params: { user: { name:  "Sonam", email: "user@valid.com", password:"foobar", password_confirmation: "foobar" } }
+    end
+
+    follow_redirect!
+    assert_template 'users/show'
+    assert_not flash.empty?
+  end
+end
